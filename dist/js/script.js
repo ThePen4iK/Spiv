515b34483bf5c3df9cf8800dcfcfd1630d301626
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2022-03-30 18:18:31 +0300

    add fixed header

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
     let burger = document.querySelector(".burger");
     let header = document.querySelector('.header');
     // let burgerActive = document.querySelector(".clicked");
+    let startScreen = document.querySelector('.js-main');
+    let headerFixed = startScreen.offsetHeight;
+    let bodyDontScroll = document.body;
+
+    window.onscroll = function () {
+        fixed();
+    }
+
+    function fixed() {
+        //&& window.pageYOffset >= headerFixed
+        if (window.scrollY > 0) {
+            header.classList.add("header__fixed");
+        } else {
+            header.classList.remove("header__fixed");
+        }
+    }
+
+
     burger.addEventListener("click", ()=>{
         burger.classList.toggle("clicked")
         header.classList.toggle("header--open");
