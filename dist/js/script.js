d23c087c042486e4af78ccbd5dcc73fefbfb0bb9
ElenaKiseljova <e.a.kiseljova@gmail.com>
2022-04-06 12:04:13 +0300

    - поправила мобильное меню

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,33 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const rootElement = document.documentElement;
 
-    let burger = document.querySelector(".burger");
-    let header = document.querySelector('.header');
-    // let burgerActive = document.querySelector(".clicked");
-    let startScreen = document.querySelector('.js-main');
-    let headerFixed = startScreen.offsetHeight;
-    let bodyDontScroll = document.body;
+  let burger = document.querySelector(".burger");
+  let header = document.querySelector('.header');
+  // let burgerActive = document.querySelector(".clicked");
+  let startScreen = document.querySelector('.js-main');
+  let headerFixed = startScreen.offsetHeight;
+  let bodyDontScroll = document.body;
 
-    window.onscroll = function () {
-        fixed();
+  window.onscroll = function () {
+    fixed();
+  }
+
+  function fixed() {
+    //&& window.pageYOffset >= headerFixed
+    if (window.scrollY > 0) {
+      header.classList.add("header__fixed");
+    } else {
+      header.classList.remove("header__fixed");
     }
-
-    function fixed() {
-        //&& window.pageYOffset >= headerFixed
-        if (window.scrollY > 0) {
-            header.classList.add("header__fixed");
-        } else {
-            header.classList.remove("header__fixed");
-        }
-    }
+  }
 
 
-    burger.addEventListener("click", ()=>{
-        burger.classList.toggle("clicked")
-        header.classList.toggle("header--open");
-    })
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("clicked")
+    header.classList.toggle("header--open");
+    rootElement.classList.toggle('block');
+  })
 
 
-    console.log("DOM fully loaded and parsed");
+  console.log("DOM fully loaded and parsed");
 })
 document.addEventListener("DOMContentLoaded",(function(){console.log("DOM fully loaded and parsed")}));
 /**
