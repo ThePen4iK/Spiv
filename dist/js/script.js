da1ffb6ac924c9d234bb8d14873fa2e20d9d2870
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2022-05-03 20:02:10 +0300

    video mobile test-3

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,35 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const rootElement = document.documentElement;
+    const rootElement = document.documentElement;
 
-  let burger = document.querySelector(".burger");
-  let header = document.querySelector('.header');
-  // let burgerActive = document.querySelector(".clicked");
-  let startScreen = document.querySelector('.js-main');
-  let headerFixed = startScreen.offsetHeight;
-  let bodyDontScroll = document.body;
+    let burger = document.querySelector(".burger");
+    let header = document.querySelector('.header');
+    // let burgerActive = document.querySelector(".clicked");
+    let startScreen = document.querySelector('.js-main');
+    let headerFixed = startScreen.offsetHeight;
+    let bodyDontScroll = document.body;
 
-  window.onscroll = function () {
-    fixed();
-  }
-
-  function fixed() {
-    //&& window.pageYOffset >= headerFixed
-    if (window.scrollY > 0) {
-      header.classList.add("header__fixed");
-    } else {
-      header.classList.remove("header__fixed");
+    window.onscroll = function () {
+        fixed();
     }
-  }
+
+    function fixed() {
+        //&& window.pageYOffset >= headerFixed
+        if (window.scrollY > 0) {
+            header.classList.add("header__fixed");
+        } else {
+            header.classList.remove("header__fixed");
+        }
+    }
 
 
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("clicked")
-    header.classList.toggle("header--open");
-    rootElement.classList.toggle('block');
-  })
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("clicked")
+        header.classList.toggle("header--open");
+        rootElement.classList.toggle('block');
+    })
 
-
-  console.log("DOM fully loaded and parsed");
+    const video = document.querySelector("video");
+    if (video) {
+        video.play();
+        console.log(1)
+    }
+    console.log("DOM fully loaded and parsed");
 })
 document.addEventListener("DOMContentLoaded",(function(){console.log("DOM fully loaded and parsed")}));
 /**
