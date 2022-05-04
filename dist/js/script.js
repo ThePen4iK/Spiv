de2517a0c40a59202cf6b67aa4286e96300be600
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2022-05-04 15:31:44 +0300

    video mobile test-10

--- dist/js/script.js
+++ dist/js/script.js
@@ -306,27 +306,27 @@ document.addEventListener("DOMContentLoaded", function () {
     if (video) {
         video.play();
     }
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
-    console.log(isIOS)
-
-    if (isIOS) {
-
-        const canvasVideo = new CanvasVideoPlayer({
-            videoSelector: 'video',
-            canvasSelector: '.canvas',
-            timelineSelector: false,
-            autoplay: true,
-            makeLoop: true,
-            pauseOnClick: false,
-            audio: false
-        });
-
-    }else {
-
-        // Use HTML5 video
-        document.querySelectorAll('.canvas')[0].style.display = 'none';
-
-    }
+    // const isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+    // console.log(isIOS)
+    //
+    // if (isIOS) {
+    //
+    //     const canvasVideo = new CanvasVideoPlayer({
+    //         videoSelector: 'video',
+    //         canvasSelector: '.canvas',
+    //         timelineSelector: false,
+    //         autoplay: true,
+    //         makeLoop: true,
+    //         pauseOnClick: false,
+    //         audio: false
+    //     });
+    //
+    // }else {
+    //
+    //     // Use HTML5 video
+    //     document.querySelectorAll('.canvas')[0].style.display = 'none';
+    //
+    // }
 
     console.log("DOM fully loaded and parsed");
 })
